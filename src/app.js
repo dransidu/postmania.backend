bb11ce0ff7fdf42db1878b7962523e3d4ef8c37f
dransidu <Dinitha@DESKTOP-04DT3TB>
2022-05-15 09:20:44 +0530

    Adding api/v1 routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const v1 = require('./api/v1/routes')
 
 // port 
 const PORT = 5000;
+
+// config routes
+app.use('/api/v1', v1);
 
 // app listen
 app.listen(PORT, () => { console.log(`Server start on ${PORT} port`) })
